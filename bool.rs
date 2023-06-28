6814f7e3240ad30c444259382f8dfdf2e627594c
aondenet-sinval <sinvalsatv@gmail.com>
2023-06-28 08:36:41 -0300

    first commit

---
+++ bool.rs
@@ -0,0 +1,5 @@
+fn main(){
+    // Declare variable to store result of "greater than" test, Is 1 > 4? -- false
+    let is_bigger = 1 > 4;
+    println!("Is 1 > 4? {}", is_bigger);//Is 1 > 4? false
+}
---
+++ tipos.rs
@@ -0,0 +1,4 @@
+fn main(){
+    let number: u32 = 14;//tipo number declarado como inteiro de 32 bits
+    println!("The number is {}.", number);//The number is 14.
+}
